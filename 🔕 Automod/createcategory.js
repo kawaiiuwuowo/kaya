89509fb5d27b32a55e111572aa5ae6ef16101717
DevCuddly <85348865+kawaiiuwuowo@users.noreply.github.com>
2023-05-16 13:29:38 +0200

    Create createcategory.js

---
+++ 🔕 Automod/createcategory.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const { Permissions } = require('discord.js')
+const { emojis } = require('../../assets.json')
+
+module.exports = {
+  name: "createcategory",
+  description: "Create a category channel!",
+  usage: "createcategory <name>",
+  cooldown: 0,
+  aliases: ["ccategory"],
+  run: async (client, message, args) => {
+    if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS))
+      return message.channels.send({
+        content:
+          "You need the `MANAGE_CHANNELS` permission to use this command"
+      });
+    const name = args[0];
+    if (!name)
+      return message.channel.send({
+        content:
+          `${emojis.wrong} You need to provide a name!`
+      });
+
+    const newChannel = message.guild.channels.create(`${name}`, {
+      type: "GUILD_CATEGORY",
+      reason: `requested by ${message.author.tag}`,
+    });
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Channel Created!`)
+      .setDescription(`Created a category channel with the name: ${name}`)
+      .setColor(client.color)
+
+    message.channel.send({embeds: [embed]});
+  },
+};
