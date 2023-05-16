27d1afbed6c4d75804de1a9b9349f17fca156bfd
DevCuddly <85348865+kawaiiuwuowo@users.noreply.github.com>
2023-05-16 13:51:09 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,47 @@
+exports.Color = `#2C2F33`;
+
+
+
+exports.Color = `BLURPLE`;
+
+
+exports.BotName= `!devcuddly#7378`;
+
+exports.PREFIX = '.';
+
+exports.GOOGLE_API_KEY = "AIzaSyBxgUhgj3sNvgJRJKBoiebroYS5LW2rj5s";
+
+exports.Default_Prefix = ".";
+
+module.exports = {
+  guildId: "",
+  verifyRole: "",
+  verifyChannel: "",
+    "ServerID": ""
+
+
+};
+
+
+  exports.BotName= ``;
+
+// Your bot ID:
+exports.BotID= ``;
+
+exports.ServerName= ``;
+
+exports.Minecraft
+
+exports.PREFIX = ".";
+
+exports.OWNER_ID = "" 
+
+
+module.exports = {
+    //CHANGE ALL OF THESE
+    channelid: "",
+    botname: "",
+  roleid: ""
+}
+   
+
